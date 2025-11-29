396ffdf44326e9551a9edb23e833be3c34bd6fa7
Takeo140 <takeoyamamoto0@gmail.com>
2025-11-29 12:14:19 +0900

    Delete test_axioms.py

--- test_axioms.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from ftheory.axioms import AxiomConsistency
-class DummyLaw:
-    def is_consistent(self):
-        return True
-
-def test_axiom_consistency():
-    a = AxiomConsistency()
-    assert a.check([DummyLaw(), DummyLaw()])
